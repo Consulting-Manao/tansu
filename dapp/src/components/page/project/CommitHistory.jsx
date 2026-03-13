tupui/soroban-versioning

feat: add support for non-software projects

dapp/src/components/page/project/CommitHistory.jsx
@@ -44,11 +44,11 @@ const CommitHistory = () => {
       const configData = loadConfigData();
       if (
         configData &&
-        configData.authorGithubNames &&
-        Array.isArray(configData.authorGithubNames) &&
-        configData.authorGithubNames.length > 0
+        configData.authorHandles &&
+        Array.isArray(configData.authorHandles) &&
+        configData.authorHandles.length > 0
       ) {
-        const authors = configData.authorGithubNames
+        const authors = configData.authorHandles
           .map((name) =>
             name && typeof name === "string" ? name.toLowerCase() : "",
           )

Consulting-Manao/tansu

dapp/src/components/page/project/CommitHistory.jsx
@@ -61,12 +61,12 @@ const CommitHistory = () => {
     try {
       const cfg = loadConfigData();
       if (
-        cfg &&
-        cfg.authorGithubNames &&
-        Array.isArray(cfg.authorGithubNames) &&
-        cfg.authorGithubNames.length > 0
+        configData &&
+        configData.authorHandles &&
+        Array.isArray(configData.authorHandles) &&
+        configData.authorHandles.length > 0
       ) {
-        const authorList = cfg.authorGithubNames
+        const authorList = configData.authorHandles
           .map((name) =>
             name && typeof name === "string" ? name.toLowerCase() : "",
           )